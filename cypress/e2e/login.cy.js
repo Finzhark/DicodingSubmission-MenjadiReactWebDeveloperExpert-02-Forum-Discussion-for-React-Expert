94b7f3338c58c2bd047eb348b5276d03ceb26381
Finzhark <cv.finza@gmail.com>
2024-06-29 03:24:08 +0700

    menginstall, memodifikasi package.json + cypress.config.js, dan testing login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,42 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('Login', () => {
+  it('it should email or password not be empty', () => {
+    cy.once('uncaught:exception', () => false);
+
+    cy.visit('http://localhost:5173/');
+    cy.get('[data-cy=login]').click();
+    cy.get('[data-cy=button-login]').click();
+    cy.on('window:alert', (text) => {
+      expect(text).to.equal('"email is not allowed to be empty');
+    });
+  });
+  it('it should password not be empty', () => {
+    cy.once('uncaught:exception', () => false);
+    cy.visit('http://localhost:5173/');
+    cy.get('[data-cy=login]').click();
+    cy.get('[data-cy=email]').type('e242090@dicoding.org');
+    cy.get('[data-cy=button-login]').click();
+    cy.on('window:alert', (text) => {
+      expect(text).to.equal('"password is not allowed to be empty');
+    });
+  });
+  it('it should email or password wrong', () => {
+    cy.once('uncaught:exception', () => false);
+    cy.visit('http://localhost:5173/');
+    cy.get('[data-cy=login]').click();
+    cy.get('[data-cy=email]').type('e242090@dicoding.org');
+    cy.get('[data-cy=password]').type('saminaminaeewakawakaee');
+    cy.get('[data-cy=button-login]').click();
+    cy.on('window:alert', (text) => {
+      expect(text).to.equal('"email or password is wrong"');
+    });
+  });
+  it('it should login successfully', () => {
+    cy.once('uncaught:exception', () => false);
+    cy.visit('http://localhost:5173/');
+    cy.get('[data-cy=login]').click();
+    cy.get('[data-cy=email]').type('e242090@dicoding.org');
+    cy.get('[data-cy=password]').type('testing');
+    cy.get('[data-cy=button-login]').click();
+    cy.get('[data-cy=logout]').click();
+  });
+});
