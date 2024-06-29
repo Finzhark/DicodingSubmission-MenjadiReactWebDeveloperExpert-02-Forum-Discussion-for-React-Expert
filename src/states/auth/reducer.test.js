25de804da4f59a41302e7a8a4ff90c0179bbbf7e
Finzhark <cv.finza@gmail.com>
2024-06-29 16:40:22 +0700

    menuliskan skenario test untuk auth action + reducer

--- src/states/auth/reducer.test.js
+++ src/states/auth/reducer.test.js
@@ -1,3 +1,17 @@
+/**
+ * skenario test
+ *
+ * - auth reducer
+ *   - should return the initial state
+ *     - should return the initial state when no action is dispatched
+ *   - should handle LOGIN_SUCCESS
+ *     - should set isLogin to true when LOGIN_SUCCESS action is dispatched
+ *   - should handle SET_PROFILE
+ *     - should set profile when SET_PROFILE action is dispatched
+ *   - should handle LOGOUT
+ *     - should reset isLogin to false and clear profile when LOGOUT action is dispatched
+ */
+
 import { describe, expect, it } from 'vitest';
 import authReducer from './reducer';
 import api from '../../services/api';
