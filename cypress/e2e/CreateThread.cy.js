dea77a1642193a05e865e66a5ba7a72860b814c6
Finzhark <cv.finza@gmail.com>
2024-06-29 16:37:22 +0700

    membuat skenario test untuk CreateThread.cy.js

--- cypress/e2e/CreateThread.cy.js
+++ cypress/e2e/CreateThread.cy.js
@@ -1,21 +1,39 @@
+/**
+ * skenario test
+ *
+ * - CreateThread
+ *   - should create new thread successfully
+ *     - visit the homepage
+ *     - click on login button
+ *     - enter email and password
+ *     - click on login button and handle alert
+ *     - wait for page to load and ensure add thread button is visible
+ *     - click on add thread button
+ *     - enter title, body, and category for the new thread
+ *     - click on create thread button
+ *     - logout after creating thread
+ */
+
 describe('CreateThread', () => {
-  it('it should create new thread successfully', () => {
+  it('should create new thread successfully', () => {
     cy.once('uncaught:exception', () => false);
     cy.visit('http://localhost:5173/');
     cy.get('[data-cy=login]').click();
     cy.get('[data-cy=email]').type('e242090@dicoding.org');
     cy.get('[data-cy=password]').type('testing');
     cy.get('[data-cy=button-login]').click();
-    cy.on('window:alert', (havtext) => {
+    cy.on('window:alert', (text) => {
       expect(text).to.equal('Login Success');
-      expect(text).to.equal('Ok').click();
     });
+    cy.wait(500);
+    cy.get('button').contains('OK').click();
     cy.wait(500);
     cy.get('[data-cy=add-thread]').should('be.visible').click();
     cy.get('[data-cy=title]').type('Ohayou Gozaimasu');
-    cy.get('[data-cy=body-thread]').type('Shiawasena');
-    cy.get('[data-cy=category]').type('Shiawasena');
+    cy.get('[data-cy=body-thread]').type('Watashi wa mahou shoujo!');
+    cy.get('[data-cy=category]').type('Halusin Nasi');
     cy.get('[data-cy=button-create-thread]').click();
+    cy.wait(3000);
     cy.get('[data-cy=logout]').click({ force: true });
   });
 });
